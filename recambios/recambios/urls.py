13f5d050f981c99c48be83059679dba0bf5b1f5c
Jaretz <bmcx12@gmail.com>
2016-03-01 13:44:53 +0100

    Finished html, css & js imports.

--- recambios/recambios/urls.py
+++ recambios/recambios/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'main.views.index', name='index'),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
